c88ddaf37cbce2b51a2a199b9e784284b64c0521
akshaymkhare <akshaymkhare@gmail.com>
2018-09-02 09:33:31 +0530

    bitwiseMultipleOf16.py

--- homework/20180901/bitwiseMultipleOf16.py
+++ homework/20180901/bitwiseMultipleOf16.py
@@ -8,7 +8,10 @@ Program : write a program to accept a number from user and check if it is multip
 
 def main():
 	num01 = eval(input("Enter a number : "))
-	
+	if ((num01 & 15) == 0):
+		print ("Number is divisible")
+	else:
+		print ("Number is not divisible")
 
 if __name__ == "__main__":
 	main()
